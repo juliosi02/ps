9b120858883449ec3a8aae342bda13d2c9aca1ca
Julio Hidalgo <juliojoseh810@gmail.com>
2024-04-09 14:40:46 -0400

    cargar contenido

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'PrestaServis RL';
+export const SITE_DESCRIPTION = 'Comprometida con la calidad y responsabilidad en nuestros servicios, Prestaservis RL es una empresa destinada principalmente a trabajos relacionados a la instrumentacion, electronica,  obras mecanicas y ejecucion de proyectos dentro del campo, con personal ampliamente calificado y con gran experiencia en este area';
